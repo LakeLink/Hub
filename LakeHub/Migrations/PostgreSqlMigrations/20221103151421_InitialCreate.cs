3b121cb35fe2e8246e45d02a73cb6c136f7bec51
Fly Higher <daniel_sy@live.com>
2022-11-04 19:36:20 +0800

    reset migrations

---
+++ LakeHub/Migrations/PostgreSqlMigrations/20221103151421_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LakeHub.Migrations.PostgreSqlMigrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    CasId = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    OrgId = table.Column<int>(type: "integer", nullable: false),
+                    Org = table.Column<string>(type: "text", nullable: true),
+                    Identity = table.Column<string>(type: "text", nullable: true),
+                    EmailVerified = table.Column<bool>(type: "boolean", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    IPAtEmailVerify = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.CasId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
