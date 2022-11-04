a4e9769d939f630ebd728e8a340eb8a3a7ac9e8b
Fly Higher <daniel_sy@live.com>
2022-11-04 19:55:53 +0800

    fix "Cannot create a DbSet for 'OpenIddictEntityFrameworkCoreApplication' because this type is not included in the model for the context."

--- LakeHub/Migrations/PostgreSqlMigrations/20221104120013_OpenIddict.Designer.cs
+++ LakeHub/Migrations/PostgreSqlMigrations/20221104120013_OpenIddict.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LakeHub;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LakeHub.Migrations.PostgreSqlMigrations
 {
     [DbContext(typeof(PostgreSqlDbContext))]
-    partial class PostgreSqlDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221104120013_OpenIddict")]
+    partial class OpenIddict
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
