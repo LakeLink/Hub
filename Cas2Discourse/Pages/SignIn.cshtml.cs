0534dabd06ab529b946ead4b6afc5fb0a6f38d55
Fly Higher <daniel_sy@live.com>
2023-02-08 17:55:21 +0800

    UI

--- Cas2Discourse/Pages/SignIn.cshtml.cs
+++ Cas2Discourse/Pages/SignIn.cshtml.cs
@@ -1,11 +1,9 @@
 using Cas2Discourse.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Options;
+using Refit;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography;
-using System.Text;
+using System.Net;
 using System.Text.Json;
 
 namespace Cas2Discourse.Pages
@@ -20,11 +18,16 @@ namespace Cas2Discourse.Pages
         public string? InputCasId { get; set; }
 
         [BindProperty]
+        [DataType(DataType.EmailAddress)]
         public string? InputEmail { get; set; }
 
         [BindProperty]
         [DataType(DataType.Password)]
         public string? InputPassword { get; set; }
+
+        public bool WrongId { get; set; } = false;
+        public bool WrongPassword { get; set; } = false;
+        public bool UnknownError { get; set; } = false;
 
         public SignInModel(ILogger<SignInModel> logger, ICASRestProtocol cas, DiscourseSsoProtocol sso)
         {
@@ -39,21 +42,49 @@ namespace Cas2Discourse.Pages
 
         public async Task<IActionResult> OnPost(string? sso, string? sig)
         {
-
-            JsonDocument json = await _cas.Users(new CASUsernamePasswordCredential(InputCasId!, InputPassword!));
-            var JsonPrincipleAttribs = json.RootElement.GetProperty("authentication").GetProperty("successes").GetProperty("RestAuthenticationHandler").GetProperty("principal").GetProperty("attributes");
-
-            var ret = _sso.Sign(sso!, sig!, new Dictionary<string, string?>()
+            try
             {
-                { "external_id", InputCasId },
-                { "name", JsonPrincipleAttribs.GetProperty("name").GetString()! },
-                { "email", InputEmail! },
-                { "require_activation", "true" },
-                // user_field_{id}, id can be found in the url of each custom field or through api response
-                { "custom.user_field_1", JsonPrincipleAttribs.GetProperty("organization").GetString()! }
-            });
-            _logger.LogInformation("User {CASId} logged in Discourse from {Address}", InputCasId, HttpContext.Connection.RemoteIpAddress);
-            return Redirect(ret);
+                JsonDocument json = await _cas.Users(new CASUsernamePasswordCredential(InputCasId!, InputPassword!));
+                var JsonPrincipleAttribs = json.RootElement.GetProperty("authentication").GetProperty("successes").GetProperty("RestAuthenticationHandler").GetProperty("principal").GetProperty("attributes");
+
+                var ret = _sso.Sign(sso!, sig!, new Dictionary<string, string?>()
+                        {
+                            { "external_id", InputCasId },
+                            { "name", JsonPrincipleAttribs.GetProperty("name").GetString()! },
+                            { "email", InputEmail! },
+                            { "require_activation", "true" },
+                            // user_field_{id}, id can be found in the url of each custom field or through api response
+                            { "custom.user_field_1", JsonPrincipleAttribs.GetProperty("organization").GetString()! }
+                        });
+                _logger.LogInformation("User {CASId} logged in Discourse from {Address}", InputCasId, HttpContext.Connection.RemoteIpAddress);
+                return Redirect(ret);
+            }
+            catch (ApiException e)
+            {
+                if (e.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    if (e.Content != null && e.Content.Contains("AccountNotFoundException"))
+                    {
+                        _logger.LogWarning("{Address} tried to sign in a nonexistent account {CASId}.", HttpContext.Connection.RemoteIpAddress, InputCasId);
+
+                        InputCasId = "";
+                        WrongId = true;
+                    }
+                    else
+                    {
+                        _logger.LogWarning("{Address} tried to sign in as {CASId} with wrong password.", HttpContext.Connection.RemoteIpAddress, InputCasId);
+
+                        InputPassword = "";
+                        WrongPassword = true;
+                    }
+                }
+                else
+                {
+                    UnknownError = true;
+                    ModelState.AddModelError(string.Empty, "Gateway returned an unexpected error.");
+                }
+                return Page();
+            }
         }
     }
 }
