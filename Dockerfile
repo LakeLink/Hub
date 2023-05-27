c170bdc7da30f9da623bde6abbde2c2ee025b632
Yiffyi Jia <daniel_sy@live.com>
2023-05-28 00:08:38 +0800

    set: session password from env

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Rebuild the source code only when needed
-FROM node:16-alpine AS builder
+FROM node:18-alpine AS builder
 WORKDIR /app
 COPY . .
 # Next.js collects completely anonymous telemetry data about general usage.
@@ -13,7 +13,7 @@ RUN yarn && yarn build
 # RUN npm run build
 
 # Production image, copy all the files and run next
-FROM node:16-alpine AS runner
+FROM node:18-alpine AS runner
 WORKDIR /app
 
 ENV NODE_ENV production
--- lib/session.ts
+++ lib/session.ts
@@ -1,21 +1,12 @@
 // this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 import type { IronSessionOptions } from "iron-session";
-import crypto from 'node:crypto'
 
-let globalWithRandomArray = global as typeof globalThis & {
-  _randomArray?: Uint8Array
-}
-
-if (!globalWithRandomArray._randomArray) {
-
-  const _randomArray = new Uint8Array(32)
-  crypto.getRandomValues(_randomArray)
-
-  globalWithRandomArray._randomArray = _randomArray
+if (!process.env.SESS_PWD) {
+  throw Error("Session password not set!")
 }
 
 export const sessionOptions: IronSessionOptions = {
-  password: Buffer.from(globalWithRandomArray._randomArray).toString('base64'),
+  password: process.env.SESS_PWD,
   cookieName: "lks",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
