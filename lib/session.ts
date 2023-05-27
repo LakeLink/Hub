910f27e3e7d17ad93cbaf5fdbc71cd6d76b4ffd2
Yiffyi Jia <daniel_sy@live.com>
2023-05-28 00:32:41 +0800

    fix: build error for session password

--- lib/session.ts
+++ lib/session.ts
@@ -2,11 +2,11 @@
 import type { IronSessionOptions } from "iron-session";
 
 if (!process.env.SESS_PWD) {
-  throw Error("Session password not set!")
+  console.warn("Session password not set!")
 }
 
 export const sessionOptions: IronSessionOptions = {
-  password: process.env.SESS_PWD,
+  password: process.env.SESS_PWD ?? "complex_password_at_least_32_characters_long",
   cookieName: "lks",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
